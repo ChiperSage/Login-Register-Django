dd0152c0cf4934734e684ec818be4103d2987d23
ByteNinja <pijayblog@gmail.com>
2024-08-05 16:48:36 +0700

    Update settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -1,24 +1,9 @@
 # myapp/settings.py
 
 INSTALLED_APPS = [
-    # Other installed apps
+    # Other apps
     'accounts',
+    # ...
 ]
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'accounts.User'
